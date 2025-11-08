a4ab8a6b0d8ae877e4c0e75743a57e0c8add56d0
Jaime Roldán <jaime.roldan@opendeusto.es>
2025-11-08 17:27:09 +0100

    cambio2

--- FM2025/src/Main/Main.java
+++ FM2025/src/Main/Main.java
@@ -3,7 +3,7 @@ package Main;
 public class Main {public Main() {
 	
 	
-	System.out.println("HOLA VENEZUELA");
+	System.out.println("HOLA VENEZUELaA");
 	// TODO Auto-generated constructor stub
 }
 
